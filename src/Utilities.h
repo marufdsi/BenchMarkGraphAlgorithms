dc5a6bde5d1de7a3ca55380e4fe4052e34fd741f
Md Maruf Hossain <maruf.csea1@gmail.com>
2019-06-25 18:16:27 -0400

    This is a bench marok of graph algorithms

---
+++ src/Utilities.h
@@ -0,0 +1,11 @@
+
+/*                                                                                                                                                                                                                
+ * GraphReader.h                                                                                                                                                                                                  
+ * Created By Md Maruf Hossain                                                                                                                                                                                    
+ * Student at UNC Charlotte (06-25-2019)                                                                                                                                                                          
+ */
+
+typedef uint32_t index; // more expressive name for an index into an array
+typedef uint32_t count; // more expressive name for an integer quantity
+typedef index node; 
+typedef double edgeweight; // edge weight type
